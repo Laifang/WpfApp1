06fa5e702529a3fb6a13d0adb335f8df2eb0f5d5
芳升 赖 <451598452@qq.com>
2020-12-19 11:26:55 +0800

    第一次上传项目

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Student stu = new Student();
+            stu.SetValue(Student.NameProperty, this.box1.Text);
+            box2.Text = (string)stu.GetValue(Student.NameProperty);
+        }
+    }
+    public class Student : DependencyObject 
+    {
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name",typeof(string),typeof(Student));
+        
+    }
+}
