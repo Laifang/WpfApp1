a683e778c6133fac18516b67c63b82889c2d821c
芳升 赖 <451598452@qq.com>
2020-12-20 11:00:58 +0800

    重新复习

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,31 +19,19 @@ namespace WpfApp1
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
-    {   
-        Student stu;
+    {
+        
 
         public MainWindow()
         {
             InitializeComponent();
+           
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            //在student类实现了依赖属性和clr包装器的基础上 可以轻易地完成双向绑定
-            Student stu= new Student();
-            stu.Name=this.box1.Text;
-            this.box2.Text=stu.Name;
-            MessageBox.Show("现在stu.Name属性的值为："+(string)stu.Name);
+           
         }
     }
-    public class Student : DependencyObject 
-    {
-         
-        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name",typeof(string),typeof(Student));
-        //当Student类 使用了依赖属性和clr属性包装器，就可以实现绑定 
-        public string Name{
-            get{return (string)GetValue(NameProperty);}
-            set{SetValue(NameProperty,value);}
-        }       
-    }    
+    
 }   
---
+++ Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Student stu;
+        public Window1()
+        {
+            InitializeComponent();
+            stu = new Student();
+            //未实现 BindingExpressionBase SetBinding（）方法之前 只能用这种不太直观的方法设置绑定
+            //BindingOperations.SetBinding(stu, Student.NameProperty, new Binding("Text") { Source = box1 });
+            stu.SetBinding(Student.NameProperty, new Binding("Text") { Source = box1 });
+      
+            box2.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = stu });
+            
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(stu.GetValue(Student.NameProperty).ToString());
+        }
+    }
+    public class Student : DependencyObject
+    {
+
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Student));
+       // 当Student类 使用了依赖属性和clr属性包装器，就可以实现绑定
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+        //实现这个方法可以使得Student类 直接调用SetBinding方法,这个方法本身可以看见，也是对BidingOperations.SetBinding()的一个薄封装
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this, dp, binding);
+        }
+    }
+}
