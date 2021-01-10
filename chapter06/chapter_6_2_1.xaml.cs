cd9726ec8f7e7db595ff6c59ef8eb2b9c3c16765
芳升 赖 <451598452@qq.com>
2021-01-10 21:29:39 +0800

    更新第六章的Binding学习内容

---
+++ chapter06/chapter_6_2_1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp1.chapter06
+{
+    /// <summary>
+    /// chapter_6_2_1.xaml 的交互逻辑
+    /// </summary>
+    public partial class chapter_6_2_1 : Window
+    {
+        Student stu;
+        public chapter_6_2_1()
+        {
+            InitializeComponent();
+            //准备数据源
+            stu = new Student();
+            //准备Binding
+            var binding = new Binding
+            {
+                Source = stu,
+                Path = new PropertyPath("Name")
+            };
+
+            //使用Binding 连接数据源与Binding目标
+            BindingOperations.SetBinding(this.textBoxName, TextBox.TextProperty, binding);
+            //TextBox  本身的基类FrameWorkElement 对BindingOperations.setBinding（...)方法进行了分装，以下是简化的代码
+            //this.textBoxName.SetBinding(TextBox.TextProperty, new Binding() { Source = stu = new Student() });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //使用button按钮事件更改stu的Name的值，会看到textbox 的内容发生了改变，说明stu.Name与TextBox.Text 属性进行了绑定
+            stu.Name += "Name";
+        }
+    }
+
+    public class Student : INotifyPropertyChanged
+    {
+        private string name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged.Invoke(this,new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
---
+++ chapter06/chapter_6_3_1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.chapter06
+{
+    /// <summary>
+    /// chapter_6_3_1.xaml 的交互逻辑
+    /// </summary>
+    public partial class chapter_6_3_1 : Window
+    {
+        public chapter_6_3_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
