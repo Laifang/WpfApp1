28db3c8aa0524602fcf6ba7e2229955bd66eb565
芳升 赖 <451598452@qq.com>
2021-01-14 11:48:36 +0800

    学习了以自定义Button类的方式创建新的button，重新梳理各项知识点

---
+++ IconTextButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// IconTextButton示例，把Icon的SVG数据 设置为可灵活配置的形式，需要用到依赖属性DependencyProperty
+    /// </summary>
+    public class IconTextButton : Button
+    {
+
+
+        
+        public IconTextButton()
+        {
+        }
+
+        /// <summary>
+        /// 图标数据
+        /// </summary>
+        public Geometry IconData
+        {
+            get { return (Geometry)GetValue(IconDataProperty); }
+            set { SetValue(IconDataProperty, value); }
+        }
+        /// <summary>
+        /// <see cref="IconData"/> 这个用法需要学习一下是干什么的
+        /// </summary>
+        public static readonly DependencyProperty IconDataProperty =
+            DependencyProperty.Register(nameof(IconData), typeof(Geometry), typeof(IconTextButton));
+
+
+
+        //public string Text
+        //{
+        //    get { return (string)GetValue(TextProperty); }
+        //    set { SetValue(TextProperty, value); }
+        //}
+
+
+        ///// <summary>
+        ///// <see cref="Text"/>
+        ///// </summary>
+        //public static readonly DependencyProperty TextProperty =
+        //    DependencyProperty.Register(nameof(Text), typeof(string), typeof(IconTextButton) );
+
+
+    }
+}
